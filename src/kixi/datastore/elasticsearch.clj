965f846dbf3f494a65aeb24f7a80781d553acdf4
Tom Coupland <tom.coupland@mixrad.io>
2016-11-09 20:17:46 +0000

    Factored out ElasticSearch interactions
    Moved all the elastic search interaction code out to a new namespace
    ready for reuse by the schemastore.

---
+++ src/kixi/datastore/elasticsearch.clj
@@ -0,0 +1,162 @@
+(ns kixi.datastore.elasticsearch
+  (:require [clojurewerkz.elastisch.rest :as esr]
+            [clojurewerkz.elastisch.rest.document :as esd]
+            [clojurewerkz.elastisch.rest.index :as esi]
+            [taoensso.timbre :as timbre :refer [error info]]))
+
+(def put-opts {:consistency "default"})
+
+(def string-stored-not_analyzed
+  {:type "string"
+   :store "yes"
+   :index "not_analyzed"})
+
+(def string-analyzed
+  {:type "string"
+   :store "yes"})
+
+(def timestamp
+  {:type "string"})
+
+(def number
+  {:type "string"})
+
+(defn kw->es-format
+  [kw]
+  (if (namespace kw)
+    (str (clojure.string/replace (namespace kw) "." "_")
+         "__"
+         (name kw))
+    (name kw)))
+
+(defn es-format->kw
+  [confused-kw]
+  (let [splits (clojure.string/split (name confused-kw) #"__")]
+    (if (second splits)
+      (keyword
+       (clojure.string/replace (first splits) "_" ".")
+       (second splits))
+      confused-kw)))
+
+(defn map-all-keys
+  [f]
+  (fn mapper [m]
+    (cond
+      (map? m) (zipmap (map f (keys m))
+                       (map (fn [v]
+                              (cond
+                                (map? v) (mapper v)
+                                (list? v) (map mapper v)
+                                (vector? v) (mapv mapper v)
+                                (seq? v) (map mapper v)
+                                (symbol? v) (name v)
+                                (keyword? v) (f v)
+                                :else v))
+                            (vals m)))
+      (keyword? m) (f m)
+      :else m)))
+
+(def all-keys->es-format
+  (map-all-keys kw->es-format))
+
+(def all-keys->kw
+  (map-all-keys es-format->kw))
+
+(defn get-document-raw
+  [index-name doc-type conn id]
+  (esd/get conn
+           index-name 
+           doc-type 
+           id
+           {:preference "_primary"}))
+
+(defn get-document
+  [index-name doc-type conn id]  
+  (-> (get-document-raw index-name doc-type conn id)
+      :_source
+      all-keys->kw))
+
+(defn get-document-key
+  [index-name doc-type conn id k]
+  (-> (get-document-raw index-name doc-type conn id)
+      :_source
+      (get (keyword (kw->es-format k)))
+      all-keys->kw))
+
+(defn ensure-index
+  [index-name doc-type doc-def conn]
+  (when-not (esi/exists? conn index-name)
+    (esi/create conn 
+                index-name
+                {:mappings {doc-type 
+                            {:properties (all-keys->es-format doc-def)}}
+                 :settings {}})))
+
+(def apply-attempts 10)
+
+(defn version-conflict
+  [resp]
+  (some
+   #(= "version_conflict_engine_exception"
+       (:type %))
+   ((comp :root_cause :error) resp)))
+
+(defn error?
+  [resp]
+  (:error resp))
+
+(defn apply-func
+  ([index-name doc-type conn id f]
+   (loop [tries apply-attempts]
+     (let [curr (get-document index-name doc-type conn id)]
+       (let [resp (esd/put conn
+                           index-name
+                           doc-type
+                           id
+                           (f curr) 
+                           (merge put-opts
+                                  (when (:_version curr)
+                                    {:version (:_version curr)})))]
+         (if (and (version-conflict resp)
+                  (pos? tries))
+           (recur (dec tries))
+           resp))))))
+
+(defn merge-data
+  [index-name doc-type conn id update]
+  (let [es-u (all-keys->es-format update)
+        r (apply-func
+           index-name
+           doc-type
+           conn
+           id   
+           (fn [curr]
+             (merge-with merge
+                         (:_source curr)
+                         es-u)))]
+    (if (error? r)
+      (error "Unable to merge data for id: " id ". Trying to merge: " es-u ". Response: " r)
+      r)))
+
+(defn cons-data
+  [index-name doc-type conn id k element]
+  (let [r (apply-func
+           index-name
+           doc-type
+           conn
+           id   
+           (fn [curr]
+             (update (:_source curr) (kw->es-format k) 
+                     #(cons (all-keys->es-format element) %))))]
+    (if (error? r)
+      (error "Unable to cons data for id: " id ". Trying to update: " k ". Response: " r)
+      r)))
+
+(defn present?
+  [index-name doc-type conn id]
+  (esd/present? conn index-name doc-type id))
+
+(defn connect
+  [host port]
+  (esr/connect (str "http://" host ":" port)
+               {:connection-manager (clj-http.conn-mgr/make-reusable-conn-manager {:timeout 10})}))
---
+++ src/kixi/datastore/schemastore/elasticsearch.clj
@@ -0,0 +1,147 @@
+(ns kixi.datastore.schemastore.elasticsearch
+  (:require [clojure.spec :as s]
+            [clojure.data :as data]
+            [com.stuartsierra.component :as component]
+            [kixi.datastore.communications :refer [Communications]]
+            [kixi.datastore.elasticsearch :as es :refer [all-keys->kw ensure-index kw->es-format string-stored-not_analyzed string-analyzed]]
+            [kixi.datastore.schemastore :refer [SchemaStore] :as ss]
+            [kixi.datastore.schemastore.conformers :as conformers]
+            [kixi.comms :as c]
+            [kixi.datastore.transit :as t]
+            [kixi.datastore.time :as time]
+            [taoensso.timbre :as timbre :refer [error info infof debug fatal]]))
+
+(def index-name "kixi-datastore_schema-data")
+(def doc-type "schema-data")
+
+(def doc-def
+  {::ss/id string-stored-not_analyzed
+   ::ss/name string-analyzed
+   ::ss/timestamp es/timestamp
+   ::ss/schema {:type "nested"
+                :properties {::ss/tag string-stored-not_analyzed
+                             ::ss/type string-stored-not_analyzed
+                             ::ss/id string-stored-not_analyzed
+                             ::ss/min es/number
+                             ::ss/max es/number
+                             ::ss/pattern string-stored-not_analyzed
+                             ::ss/elements string-stored-not_analyzed
+                             ::ss/definition {:type "nested"
+                                              :properties {::ss/type string-stored-not_analyzed
+                                                           ::ss/id string-stored-not_analyzed
+                                                           ::ss/min es/number
+                                                           ::ss/max es/number
+                                                           ::ss/pattern string-stored-not_analyzed
+                                                           ::ss/elements string-stored-not_analyzed}}}}})
+
+(def merge-data
+  (partial es/merge-data index-name doc-type))
+
+(def cons-data
+  (partial es/cons-data index-name doc-type))
+
+(def get-document
+  (partial es/get-document index-name doc-type))
+
+(def get-document-key
+  (partial es/get-document-key index-name doc-type))
+
+(def present?
+  (partial es/present? index-name doc-type))
+
+(defn inject-tag
+  [def]
+  (reduce
+   (fn [acc [tag def]]
+     (conj acc
+           (assoc def
+                  ::ss/tag tag)))
+   []
+   (partition 2 def)))
+
+(defn inject-tags
+  [schema]
+  (if (get-in schema [::ss/schema ::ss/definition])
+    (update-in schema 
+               [::ss/schema ::ss/definition]
+               inject-tag)
+    schema))
+
+(defn extract-tag
+  [stored-def]
+  (mapcat
+   (fn [def]
+     [(::ss/tag def)
+      (dissoc def
+              ::ss/tag)])
+   stored-def))
+
+(defn extract-tags
+  [stored-schema]
+  (if (get-in stored-schema [::ss/schema ::ss/definition])
+    (update-in stored-schema
+               [::ss/schema ::ss/definition]
+               extract-tag)
+    stored-schema))
+
+(defn persist-new-schema
+  [conn schema]  
+  (let [id (::ss/id schema)
+        schema' (assoc schema ::ss/timestamp (time/timestamp))]
+    (if (s/valid? ::ss/stored-schema schema')
+      (merge-data conn id (inject-tags schema'))
+      (error "Tried to persist schema but it was invalid:" schema' (s/explain-data ::ss/stored-schema schema')))))                               ;should be at the command level
+
+(defn sub-map
+  [f s]
+  (-> (data/diff f s)
+      first
+      not))
+
+(defn fetch-with-sub-spec
+  [data sub-spec]
+  (some->> (vals @data)
+           (some #(when (sub-map sub-spec %) %))))
+
+(defn response-event
+  [r]
+  nil)
+
+(defrecord ElasticSearch
+    [communications host port conn]
+    SchemaStore
+    (authorised
+      [_ action id user-groups]
+      (when-let [sharing (get-document-key conn id ::ss/sharing)]
+        (not-empty (clojure.set/intersection (set (get sharing action))
+                                             (set user-groups)))))
+    (exists [_ id]
+      (present? conn id))
+    (fetch-with [_ sub-spec]
+      (prn "fetching: " sub-spec)
+;      (fetch-with-sub-spec data sub-spec)
+      )
+    (fetch-spec [_ id]
+      (extract-tags
+       (get-document conn id)))
+    component/Lifecycle
+    (start [component]
+      (if-not conn
+        (let [connection (es/connect host port)]
+          (info "Starting Schema ElasticSearch Store")
+          (ensure-index index-name
+                        doc-type 
+                        doc-def
+                        connection)
+          (c/attach-event-handler! communications
+                                   :kixi.datastore/schemastore
+                                   :kixi.datastore/schema-created
+                                   "1.0.0"
+                                   (comp response-event (partial persist-new-schema connection) :kixi.comms.event/payload))      
+          (assoc component :conn connection))
+        component))
+    (stop [component]
+      (if conn
+        (do (info "Destroying Schema ElasticSearch Store")
+            (dissoc component :conn))
+        component)))
