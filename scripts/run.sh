f52adab9b053ac44275ea6ad9236289d0c60060b
Tom Coupland <tcoupland@gmail.com>
2017-01-27 15:04:33 +0000

    Change to gc log location

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,5 @@ set -o xtrace
 JAR_LOCATION=${1:-$JAR_LOCATION}
 CONFIG_PROFILE=${2:-$CONFIG_PROFILE}
 
-exec java -XX:+HeapDumpOnOutOfMemoryError -XX:+UseG1GC -Xloggc:gc.log -XX:+PrintGCCause -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=2M ${JAVA_OPTS:-} -cp $JAR_LOCATION kixi.datastore.bootstrap $CONFIG_PROFILE >&1
+exec java -XX:+HeapDumpOnOutOfMemoryError -XX:+UseG1GC -Xloggc:./gc.log -XX:+PrintGCCause -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=2M ${JAVA_OPTS:-} -cp $JAR_LOCATION kixi.datastore.bootstrap $CONFIG_PROFILE >&1
 
