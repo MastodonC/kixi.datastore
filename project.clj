5d812ccbbe94da177a68e22332fb50b90a38a113
Tom Coupland <tcoupland@gmail.com>
2017-03-23 14:46:01 +0000

    Upgrading kixi.comms to 0.2.4

--- project.clj
+++ project.clj
@@ -31,7 +31,7 @@
                  [com.taoensso/encore "2.90.1"]
                  [digest "1.4.4"]
                  [environ "1.1.0"]
-                 [kixi/kixi.comms "0.2.2"]
+                 [kixi/kixi.comms "0.2.4"]
                  [kixi/kixi.log "0.1.3"]
                  [kixi/kixi.metrics "0.4.0"]
                  [kixi/joplin.core "0.3.10-SNAPSHOT"]
