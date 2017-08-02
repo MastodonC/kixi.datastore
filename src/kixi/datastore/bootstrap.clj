e0a243b995df18f2587b07d7201e8ba113f04f94
Antony Woods <antony@mastodonc.com>
2017-08-02 14:59:21 +0100

    Add sigterm catch to gracefully shutdown

--- src/kixi/datastore/bootstrap.clj
+++ src/kixi/datastore/bootstrap.clj
@@ -1,10 +1,11 @@
 (ns kixi.datastore.bootstrap
   (:require [kixi.datastore.system]
             [com.stuartsierra.component :as component]
-            [taoensso.timbre :as log])
+            [taoensso.timbre :as log]
+            [signal.handler :refer [with-handler]])
   (:gen-class))
 
-(defn -main 
+(defn -main
   [& args]
   (let [config-profile (keyword (first args))
         system (kixi.datastore.system/new-system config-profile)]
@@ -15,4 +16,7 @@
       (component/start-system system)
       (.. (Thread/currentThread) join)
       (catch Throwable t
-                 (log/error t "Top level exception caught")))))
+        (log/error t "Top level exception caught")))
+    (with-handler :term
+      (log/info "SIGTERM was caught: shutting down...")
+      (component/stop system))))
