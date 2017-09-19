0dd36418494dd120bc8ec10dab7ef688cf0c4542
Tom Coupland <tcoupland@gmail.com>
2017-09-19 09:09:38 +0100

    Adding remove all sharing kaylee command

--- src/kixi/datastore/kaylee.clj
+++ src/kixi/datastore/kaylee.clj
@@ -7,6 +7,8 @@
 
 (sh/alias 'ke 'kixi.comms.event)
 
+(def kaylee-user-group "00000000-0000-0000-0000-000000000000")
+
 
 ;; This namespace is for useful functions, designed too make ops a bunch easier
 
@@ -22,17 +24,17 @@
 
 (defn comms
   []
-  (:communications @kixi.datastore.application/system))
-
+  (:communications (system)))
 
 (defn get-metadata
   [meta-id]
+  {:pre [string?]}
   (ms/retrieve (metadatastore) meta-id))
 
 (defn send-sharing-update
   "Issues an event with sharing matrix changes, bypasses the command level user authorization."
-  [user-id user-group metadata-id change-type activity target-group]
-  {:pre [string? string? string? keyword? keyword? string?]}
+  [your-user-id metadata-id change-type activity target-group]
+  {:pre [string? string? keyword? keyword? string?]}
   (c/send-event!
    (comms)
    :kixi.datastore.file-metadata/updated
@@ -42,6 +44,15 @@
     ::ms/sharing-update change-type
     ::ms/activity activity
     :kixi.group/id target-group
-    :kixi/user {:kixi.user/id user-id
-                :kixi.user/groups [user-group]}}
+    :kixi/user {:kixi.user/id your-user-id
+                :kixi.user/groups [kaylee-user-group]}}
    {::ke/partition-key metadata-id}))
+
+(defn remove-all-sharing
+  "Issues events for all entries in the sharing matrix for the metadata-id."
+  [your-user-id metadata-id]
+  {:pre [string? string? string?]}
+  (let [metadata (get-metadata metadata-id)]
+    (doseq [activity (keys (::ms/sharing metadata))
+            target-group (get-in metadata [::ms/sharing activity])]
+      (send-sharing-update your-user-id metadata-id ::ms/sharing-disj activity target-group))))
--- test/kixi/integration/kaylee_test.clj
+++ test/kixi/integration/kaylee_test.clj
@@ -29,10 +29,30 @@
     (when-success metadata-response
       (let [meta-id (get-in metadata-response [:body ::ms/id])
             new-group (uuid)
-            event (kaylee/send-sharing-update uid uid meta-id ::ms/sharing-conj ::ms/meta-read new-group)]
+            event (kaylee/send-sharing-update uid meta-id ::ms/sharing-conj ::ms/meta-read new-group)]
         (wait-for-pred #(let [metadata (get-metadata uid meta-id)]
                           (= #{uid new-group}
                              (set (get-in metadata [:body ::ms/sharing ::ms/meta-read])))))
         (let [updated-metadata (get-metadata uid meta-id)]
           (is (= #{uid new-group}
                  (set (get-in updated-metadata [:body ::ms/sharing ::ms/meta-read])))))))))
+
+(deftest kaylee-remove-all-sharing
+  (let [uid (uuid)
+        metadata-response (send-file-and-metadata
+                           (create-metadata
+                            uid
+                            "./test-resources/metadata-one-valid.csv"))]
+    (when-success metadata-response
+      (let [meta-id (get-in metadata-response [:body ::ms/id])
+            _ (kaylee/remove-all-sharing uid meta-id)]
+        (wait-for-pred #(let [metadata (get-metadata uid meta-id)]
+                          (= #{}
+                             (set (get-in metadata [:body ::ms/sharing ::ms/meta-read])))))
+        (let [updated-metadata (get-metadata uid meta-id)]
+          (is (= #{}
+                 (set (get-in updated-metadata [:body ::ms/sharing ::ms/meta-read]))))
+          (is (= #{}
+                 (set (get-in updated-metadata [:body ::ms/sharing ::ms/meta-update]))))
+          (is (= #{}
+                 (set (get-in updated-metadata [:body ::ms/sharing ::ms/file-read])))))))))
