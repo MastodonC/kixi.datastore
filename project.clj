ae572c903e5871ef83c1a0fc7a2586b6f7c0e495
Tom Coupland <tcoupland@gmail.com>
2017-11-10 11:20:59 +0000

    Upgrading kixi.log

--- project.clj
+++ project.clj
@@ -34,7 +34,7 @@
                  [digest "1.4.6"]
                  [environ "1.1.0"]
                  [kixi/kixi.comms "0.2.27"]
-                 [kixi/kixi.log "0.1.4"]
+                 [kixi/kixi.log "0.1.5"]
                  [kixi/kixi.metrics "0.4.1"]
                  [kixi/joplin.core "0.3.10-SNAPSHOT"]
                  [kixi/joplin.dynamodb "0.3.10-SNAPSHOT"]
