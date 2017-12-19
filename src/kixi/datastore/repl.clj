f937070bd8b93f6db1dae0217b856d368f14ad50
Tom Coupland <tcoupland@gmail.com>
2017-12-19 13:59:35 +0000

    Prevent NPE on system shutdown

--- src/kixi/datastore/repl.clj
+++ src/kixi/datastore/repl.clj
@@ -4,13 +4,19 @@
             [clojure.tools.nrepl.server :as nrepl-server]
             [taoensso.timbre :as log]))
 
-(defrecord ReplServer [port]
+(defrecord ReplServer
+    [port repl-server]
   component/Lifecycle
   (start [this]
-    (prn "Starting REPL server - port " port)
-    (assoc this :repl-server
-           (nrepl-server/start-server :handler cider-nrepl-handler :bind "0.0.0.0" :port port)))
+    (if-not (:repl-server this)
+      (do
+        (log/info "Starting REPL server on" port)
+        (assoc this :repl-server
+               (nrepl-server/start-server :handler cider-nrepl-handler :bind "0.0.0.0" :port port)))
+      this))
   (stop [this]
-    (prn "Stopping REPL server" this)
-    (nrepl-server/stop-server (:repl-server this))
-    (dissoc this :repl-server)))
+    (if (:repl-server this)
+      (do (log/info "Stopping REPL server")
+          (nrepl-server/stop-server (:repl-server this))
+          (dissoc this :repl-server))
+      this)))
